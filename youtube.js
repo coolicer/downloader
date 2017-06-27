282e798ae201b57f8b02ceb67d3e1a39436681f5
eep <coolicer@foxmail.com>
2017-06-27 14:17:57 +0800

    add youtube download

--- youtube.js
+++ youtube.js
@@ -1,16 +1,37 @@
-var youtubedl = require('youtube-dl');
-var video;
-var filename;
+const fetchVideoInfo = require('youtube-info');
+const querystring = require('querystring');
+const cmd=require('node-cmd');
 
 function Youtube(url) {
-  video = youtubedl(url,
-  ['--format=18'],
-  { cwd: '/home/download' });
-
-  video.on('info', function(info) {
-    filename = info.filename;
-    video.pipe(fs.createWriteStream(filename));
-  });
+  let videoId;
+  let _url = url;
+  url = url.split('?')[1];
+  url = querystring.parse(url);
+  videoId = url.v;
+  videoId && fetchVideoInfo(videoId)
+    .then((videoInfo) => {
+      return videoInfo.title;
+    })
+    .then((filename) => {
+      cmdDown(_url, filename);
+    });
 }
 
+function cmdDown(url, filename) {
+  cmd.get(
+        `
+            cd /home/download
+            youtube-dl -f 18 ${url}
+        `,
+        function(err, data, stderr){
+            if (!err) {
+               console.log('the node-cmd cloned dir contains these files :\n\n',data)
+            } else {
+               console.log('error', err)
+            }
+ 
+        }
+    );
+}
+// Youtube('https://wwww.youtube.com/watch?v=m_lnmN6F34w');
 module.exports = Youtube;
